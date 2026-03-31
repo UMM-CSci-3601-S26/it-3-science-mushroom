a7dd95c32e92a825b7c7e4047559a5d7bf6d345d
Harley Hannahs <hanna537@morris.umn.edu>
2026-03-31 14:26:49 -0500

    Add drag and drop functionality
    Basic drag and drop functionality for station ordering

--- client/src/app/station-order/station-order.component.ts
+++ client/src/app/station-order/station-order.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import {CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'app-station-order',
-  imports: [],
+  imports: [
+    CdkDrag,
+    CdkDropList,
+    MatCardModule
+  ],
   templateUrl: './station-order.component.html',
   styleUrl: './station-order.component.scss',
 })
 export class StationOrderComponent {
 
+  bank = [
+    'pencils',
+    'markers',
+    'crayons'
+  ];
+
+  stationOrder = [
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
