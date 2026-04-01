0dccbc638455152a86db359aecbf528fc1ff2512
Gabriel Nelson <nel13776@morris.umn.edu>
2026-04-01 16:36:58 -0500

    Add comments.
    Add comments describing what the scss rules do and what the cypress methods are grabbing,

--- client/cypress/support/family-list.po.ts
+++ client/cypress/support/family-list.po.ts
@@ -41,27 +41,57 @@ export class FamilyListPage {
     return cy.get(this.familyCardSelector);
   }
 
+  /**
+   * Gets the dashboard element of the app when visiting the `/families` page.
+   *
+   * @returns the value of the element with the ID `.family-list-title`
+   */
   getDashboard() {
     return cy.get(this.familyDashboard)
   }
 
+  /**
+   * Gets the total number of families value in the dashboard when visiting the `/families` page.
+   *
+   * @returns the value of the element with the ID `.family-list-title`
+   */
   getTotalFamilies() {
     return cy.get(this.totalFamiliesNum)
   }
 
+  /**
+   * Gets the total number of students value in the dashboard when visiting the `/families` page.
+   *
+   * @returns the value of the element with the ID `.family-list-title`
+   */
   getTotalStudents() {
     return cy.get(this.totalStudentsNum)
   }
 
+  /**
+   * Gets the list of schools and total number of students attending when visiting the `/families` page.
+   *
+   * @returns the value of the element with the ID `.family-list-title`
+   */
   getStudentsPerGrade() {
     return cy.get(this.studentsPerGrade
     )
   }
 
+  /**
+   * Gets the list of grades and total number of student in each when visiting the `/families` page.
+   *
+   * @returns the value of the element with the ID `.family-list-title`
+   */
   getStudentsPerSchool() {
     return cy.get(this.studentsPerSchool)
   }
 
+  /**
+   * Gets all the student cards when visiting the `/families` page.
+   *
+   * @returns the value of the element with the ID `.family-list-title`
+   */
   getStudentCards() {
     return cy.get(this.studentCardSelector)
   }
