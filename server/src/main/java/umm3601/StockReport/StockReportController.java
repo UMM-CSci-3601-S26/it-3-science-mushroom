11bac416ff36f27172e16f7a4fb7bcb37f28a5b4
Thatcher Werner <werne662@morris.umn.edu>
2026-04-01 16:10:27 -0500

    Tests for StockReport Backend
    - Added tests for StockReport and StockReportController
    - Updated StockReportController's getReportById with missing
      HttpStatus.ok
    - Removed unused imports in StockReportController
    - Other misc changes to satisfy checkstyle

--- server/src/main/java/umm3601/StockReport/StockReportController.java
+++ server/src/main/java/umm3601/StockReport/StockReportController.java
@@ -2,29 +2,20 @@
 package umm3601.StockReport;
 
 // Static Imports
-import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Filters.regex;
 
 import java.io.IOException;
 // Java Imports
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 // Org Imports
-import org.bson.Document;
 import org.bson.UuidRepresentation;
-import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import org.mongojack.JacksonMongoCollection;
 
 // Com Imports
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
 
 // IO Imports
@@ -34,9 +25,9 @@ import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
 import io.javalin.http.NotFoundResponse;
 import io.javalin.http.UploadedFile;
+
 // Misc Imports
 import umm3601.Controller;
-import umm3601.StockReport.StockReport;
 
 // Controller
 public class StockReportController implements Controller {
@@ -75,6 +66,7 @@ public class StockReportController implements Controller {
     }
 
     ctx.json(report);
+    ctx.status(HttpStatus.OK);
   }
 
   // POST new report
---
+++ server/src/test/java/umm3601/StockReport/StockReportSpec.java
@@ -0,0 +1,75 @@
+// Packages
+package umm3601.StockReport;
+
+// Static Imports
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+// Org Imports
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// StockReportSpec Class
+public class StockReportSpec {
+  private static final String FAKE_ID_STRING_1 = "fakeIdOne";
+  private static final String FAKE_ID_STRING_2 = "fakeIdTwo";
+
+  private StockReport report1;
+  private StockReport report2;
+
+  // -- Test Management -- \\
+
+  @BeforeEach
+  @SuppressWarnings({ "MagicNumber" })
+  void setupEach() {
+    report1 = new StockReport();
+    report2 = new StockReport();
+
+    report1.stockReportPDF = new byte[]{0x25, 0x50, 0x44, 0x46}; // PDF file header bytes
+    report1.reportName = "Report 1";
+  }
+
+  // -- StockReport ID Tests -- \\
+
+  @Test
+  void inventoriesWithEqualIdAreEqual() {
+    report1._id = FAKE_ID_STRING_1;
+    report2._id = FAKE_ID_STRING_1;
+
+    assertTrue(report1.equals(report2));
+  }
+
+  @Test
+  void inventoriesWithDifferentIdAreNotEqual() {
+    report1._id = FAKE_ID_STRING_1;
+    report2._id = FAKE_ID_STRING_2;
+
+    assertFalse(report1.equals(report2));
+  }
+
+  @Test
+  void hashCodesAreBasedOnId() {
+    report1._id = FAKE_ID_STRING_1;
+    report2._id = FAKE_ID_STRING_1;
+
+    assertTrue(report1.hashCode() == report2.hashCode());
+  }
+
+  @SuppressWarnings("unlikely-arg-type")
+  @Test
+  void inventoriesAreNotEqualToOtherKindsOfThings() {
+    report1._id = FAKE_ID_STRING_1;
+    // an StockReport is not equal to its id even though id is used for checking equality
+    assertFalse(report1.equals(FAKE_ID_STRING_1));
+  }
+
+  @Test
+  void nullId() {
+    report1._id = null;
+    report2._id = FAKE_ID_STRING_2;
+
+    assertEquals(report1.hashCode(), 0);
+    assertFalse(report1.equals(report2));
+  }
+}
