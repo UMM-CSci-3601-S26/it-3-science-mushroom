8f636195f311ee9a419d6cc4f6d81491acdcd649
Harley Hannahs <hanna537@morris.umn.edu>
2026-04-02 23:54:25 -0500

    Add basic tests for drop method
    Tests moving elements within the same list and moving elements between the two lists

--- client/src/app/station-order/station-order.component.spec.ts
+++ client/src/app/station-order/station-order.component.spec.ts
@@ -4,58 +4,59 @@ import { of } from 'rxjs';
 import { StationOrderComponent } from './station-order.component';
 import { SupplyList } from '../supplylist/supplylist';
 import { SupplyListService } from '../supplylist/supplylist.service';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 
 describe('StationOrderComponent', () => {
   let component: StationOrderComponent;
   let fixture: ComponentFixture<StationOrderComponent>;
 
-  const testSupplyList: SupplyList[]= [
-    {
-      "school": "St. Mary's",
-      "grade": "2nd Grade",
-      "teacher": "N/A",
-      "item": "Pencil",
-      "description": "Unsharpend Yellow Ticonderoga #2 Pencil",
-      "brand": "Ticonderoga",
-      "color": "Yellow",
-      "size": "#2",
-      "type": "Unsharpened",
-      "material": "N/A",
-      "count": 1,
-      "quantity": 12,
-      "notes": "N/A"
-    },
-    {
-      "school": "St. Mary's",
-      "grade": "2nd Grade",
-      "teacher": "N/A",
-      "item": "Folder",
-      "description": "Green 2 Prong Plastic Pocket Folder",
-      "brand": "N/A",
-      "color": "Green",
-      "size": "N/A",
-      "type": "2 Prong",
-      "material": "Plastic",
-      "count": 1,
-      "quantity": 2,
-      "notes": "N/A"
-    },
-    {
-      "school": "St. Mary's",
-      "grade": "2nd Grade",
-      "teacher": "N/A",
-      "item": "Pencil Box",
-      "description": "8\" x 5\" Plastic Pencil Box",
-      "brand": "N/A",
-      "color": "N/A",
-      "size": "8\" x 5\"",
-      "type": "N/A",
-      "material": "Plastic",
-      "count": 1,
-      "quantity": 1,
-      "notes": "N/A"
-    },
-  ];
+  // const testSupplyList: SupplyList[]= [
+  //   {
+  //     "school": "St. Mary's",
+  //     "grade": "2nd Grade",
+  //     "teacher": "N/A",
+  //     "item": "Pencil",
+  //     "description": "Unsharpend Yellow Ticonderoga #2 Pencil",
+  //     "brand": "Ticonderoga",
+  //     "color": "Yellow",
+  //     "size": "#2",
+  //     "type": "Unsharpened",
+  //     "material": "N/A",
+  //     "count": 1,
+  //     "quantity": 12,
+  //     "notes": "N/A"
+  //   },
+  //   {
+  //     "school": "St. Mary's",
+  //     "grade": "2nd Grade",
+  //     "teacher": "N/A",
+  //     "item": "Folder",
+  //     "description": "Green 2 Prong Plastic Pocket Folder",
+  //     "brand": "N/A",
+  //     "color": "Green",
+  //     "size": "N/A",
+  //     "type": "2 Prong",
+  //     "material": "Plastic",
+  //     "count": 1,
+  //     "quantity": 2,
+  //     "notes": "N/A"
+  //   },
+  //   {
+  //     "school": "St. Mary's",
+  //     "grade": "2nd Grade",
+  //     "teacher": "N/A",
+  //     "item": "Pencil Box",
+  //     "description": "8\" x 5\" Plastic Pencil Box",
+  //     "brand": "N/A",
+  //     "color": "N/A",
+  //     "size": "8\" x 5\"",
+  //     "type": "N/A",
+  //     "material": "Plastic",
+  //     "count": 1,
+  //     "quantity": 1,
+  //     "notes": "N/A"
+  //   },
+  // ];
 
 
   beforeEach(async () => {
@@ -69,16 +70,46 @@ describe('StationOrderComponent', () => {
 
     fixture = TestBed.createComponent(StationOrderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   afterEach(() => {
     // no active HttpTestingController required when service is mocked
   });
 
-  describe('drag and drop', () => {
-    it('should have drag and drop elements in lists', () => {
+  describe('drop method', () => {
+    it('should move elements in the same list', () => {
+      const bank = ['Pencil', 'Backpack', 'Eraser'];
+      spyOn(component, 'saveStationOrder');
 
+      const mockEvent = {
+        previousContainer: { data: bank },
+        container: { data: bank},
+        previousIndex: 1,
+        currentIndex: 0
+      } as CdkDragDrop<string[]>;
+
+      component.drop(mockEvent);
+      expect(bank).toEqual(['Backpack', 'Pencil', 'Eraser']);
+      expect(component.saveStationOrder).toHaveBeenCalled();
+    });
+
+
+    it('should move elements between lists', () => {
+      const bank = ['Pencil', 'Backpack', 'Eraser'];
+      const stationOrder = ['Folder'];
+      spyOn(component, 'saveStationOrder');
+
+      const mockEvent = {
+        previousContainer: { data: bank },
+        container: { data: stationOrder},
+        previousIndex: 0,
+        currentIndex: 0
+      } as CdkDragDrop<string[]>;
+
+      component.drop(mockEvent);
+      expect(bank).toEqual(['Backpack', 'Eraser']);
+      expect(stationOrder).toEqual(['Pencil', 'Folder']);
+      expect(component.saveStationOrder).toHaveBeenCalled();
     });
   });
 
