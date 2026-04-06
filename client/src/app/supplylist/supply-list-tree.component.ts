bb530aeed9d8cae93d9b182ae8373f5fef761f25
Thatcher Werner <werne662@morris.umn.edu>
2026-04-06 12:30:19 -0500

    Initial Work on Supply List Restructure

---
+++ client/src/app/supplylist/supply-list-tree.component.ts
@@ -0,0 +1,44 @@
+// Angular Imports
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Input } from '@angular/core';
+
+/**
+ * Supply list node data with nested structure.
+ * All fields are optional.
+ * In the event some data is missing, the tree will still display what it can.
+ */
+export interface SupplyListNode {
+  school?: string;
+  grade?: string;
+  teacher?: string;
+  description?: string;
+  label?: string;
+  children?: SupplyListNode[];
+}
+
+/**
+ * Component for the Supply List Tree. Displays inventory items in a flat tree structure, grouped by School, then Grade, then Teacher.
+ * Item descriptions are the last level.
+ */
+@Component({
+  selector: 'app-supply-list-tree',
+  templateUrl: 'supply-list-tree.component.html',
+  imports: [MatTreeModule, MatButtonModule, MatIconModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SupplyListTreeComponent {
+  @Input() supplyListNodes: SupplyListNode[] = [];
+
+  childrenAccessor = (node: SupplyListNode) => node.children ?? [];
+
+  hasChild = (_: number, node: SupplyListNode) => !!node.children && node.children.length > 0;
+
+  // Add the labels to the node for displaying
+  getChildDisplay(node: SupplyListNode): string {
+    const value = node.school ?? node.grade ?? node.teacher ?? node.description;
+    return value !== undefined ? `${node.label}: ${value}` : '';
+  }
+}
