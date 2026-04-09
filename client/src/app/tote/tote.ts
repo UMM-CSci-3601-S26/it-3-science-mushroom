43188f5c5c2c6cce1475adbdecd5159242848a54
Blake <thom9170@morris.umn.edu>
2026-04-09 07:44:37 -0500

    add interfaces for totes

---
+++ client/src/app/tote/tote.ts
@@ -0,0 +1,34 @@
+export interface ToteEntry {
+  internalID: string;
+  quantity: number;
+}
+
+export interface Tote {
+  toteBarcode: string;
+  name?: string;
+  contents: ToteEntry[];
+}
+
+export interface ToteEntryView extends ToteEntry {
+  internalID: string;
+  quantity: number;
+  item?: string;
+  brand?: string;
+  color?: string;
+  size?: string;
+  type?: string;
+  material?: string;
+  description?: string;
+}
+
+export interface ToteQuantityChangeRequest {
+  internalID: string;
+  quantity: number;
+}
+
+export interface MoveToteItemRequest {
+  formToteBarcode: string;
+  toToteBarcode: string;
+  internalID: string;
+  quantity: number;
+}
