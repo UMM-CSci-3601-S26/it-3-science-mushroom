7e5b7e056f7222c5327a8a6b580454de60275abc
Blake Thompson <Thom9170@morris.umn.edu>
2026-04-14 14:12:06 -0500

    create getter and setter for status

---
+++ server/src/main/java/umm3601/Family/FamilyStatusUpdateRequest.java
@@ -0,0 +1,13 @@
+package umm3601.Family;
+
+public class FamilyStatusUpdateRequest {
+    private boolean status;
+
+    public boolean getStatus() {
+      return status;
+    }
+
+    public void setStatus(boolean status){
+      this.status = status;
+  }
+}
