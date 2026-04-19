d2490080288aac30c82a7d967edf8f1f762622e7
Gabriel Nelson <nel13776@morris.umn.edu>
2026-04-19 15:44:12 -0500

    Add karma test
    Add tests for the get time availability function when displaying information on the card. Add a test for the page change function when displaying the families based on user input.

--- client/src/app/family/family-card.component.spec.ts
+++ client/src/app/family/family-card.component.spec.ts
@@ -87,4 +87,82 @@ describe('FamilyCardComponent', () => {
   it('should be the family named Chris', () => {
     expect(component.family().guardianName).toEqual('Chris');
   });
+
+  it('should return early morning only', () => {
+    spyOn(component, 'family').and.returnValue({
+      timeAvailability: {
+        earlyMorning: true,
+        lateMorning: false,
+        earlyAfternoon: false,
+        lateAfternoon: false
+      }
+    } as Partial<Family> as Family);
+
+    expect(component.getAvailableTimes()).toBe('Early Morning');
+  });
+
+  it('should return late morning only', () => {
+    spyOn(component, 'family').and.returnValue({
+      timeAvailability: {
+        earlyMorning: false,
+        lateMorning: true,
+        earlyAfternoon: false,
+        lateAfternoon: false
+      }
+    } as Partial<Family> as Family);
+
+    expect(component.getAvailableTimes()).toBe('Late Morning');
+  });
+
+  it('should return early afternoon only', () => {
+    spyOn(component, 'family').and.returnValue({
+      timeAvailability: {
+        earlyMorning: false,
+        lateMorning: false,
+        earlyAfternoon: true,
+        lateAfternoon: false
+      }
+    } as Partial<Family> as Family);
+
+    expect(component.getAvailableTimes()).toBe('Early Afternoon');
+  });
+
+  it('should return late afternoon only', () => {
+    spyOn(component, 'family').and.returnValue({
+      timeAvailability: {
+        earlyMorning: false,
+        lateMorning: false,
+        earlyAfternoon: false,
+        lateAfternoon: true
+      }
+    } as Partial<Family> as Family);
+
+    expect(component.getAvailableTimes()).toBe('Late Afternoon');
+  });
+
+  it('should return none only', () => {
+    spyOn(component, 'family').and.returnValue({
+      timeAvailability: {
+        earlyMorning: false,
+        lateMorning: false,
+        earlyAfternoon: false,
+        lateAfternoon: false
+      }
+    } as Partial<Family> as Family);
+
+    expect(component.getAvailableTimes()).toBe('None');
+  });
+
+  it('should return multiple', () => {
+    spyOn(component, 'family').and.returnValue({
+      timeAvailability: {
+        earlyMorning: true,
+        lateMorning: true,
+        earlyAfternoon: false,
+        lateAfternoon: true
+      }
+    } as Partial<Family> as Family);
+
+    expect(component.getAvailableTimes()).toBe('Early Morning, Late Morning, Late Afternoon');
+  });
 });
