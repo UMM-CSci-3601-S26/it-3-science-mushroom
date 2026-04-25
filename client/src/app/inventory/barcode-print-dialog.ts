e21d60dc4dd656697735927ed926c8f05cd5dc78
Blake <thom9170@morris.umn.edu>
2026-04-25 16:12:29 -0500

    create basic component with other base files

---
+++ client/src/app/inventory/barcode-print-dialog.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-barcode-print-dialog',
+  templateUrl: './barcode-print-dialog.html',
+  styleUrls: ['./barcode-print-dialog.scss'],
+  imports: [
+  ]
+})
+
+export class BarcodePrintDialog {
+}
