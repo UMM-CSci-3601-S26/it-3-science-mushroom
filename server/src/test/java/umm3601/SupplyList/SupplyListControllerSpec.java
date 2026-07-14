e4f75f63f2f3516cac2bab3bef0210532fb64c2c
Thatcher Werner <werne662@morris.umn.edu>
2026-07-14 18:49:57 -0500

    Remove: Unused import in Spec file

--- server/src/test/java/umm3601/SupplyList/SupplyListControllerSpec.java
+++ server/src/test/java/umm3601/SupplyList/SupplyListControllerSpec.java
@@ -48,7 +48,6 @@ import io.javalin.http.NotFoundResponse;
 import io.javalin.json.JavalinJackson;
 import io.javalin.validation.BodyValidator;
 import io.javalin.validation.ValidationException;
-import umm3601.Inventory.Inventory;
 
 /**
  * Tests for the SupplyListController using a real MongoDB "test" database.
