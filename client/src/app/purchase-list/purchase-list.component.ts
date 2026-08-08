01bede2dcfaa5da32fde5c819102f6192399d27d
Blake <thom9170@morris.umn.edu>
2026-08-08 06:23:35 -0500

    Add: Basic condition handing for demand

--- client/src/app/purchase-list/purchase-list.component.ts
+++ client/src/app/purchase-list/purchase-list.component.ts
@@ -1,7 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit, signal } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 
+
+// Services
+import { PurchaseListService } from "./purchase-list.service";
+
+// Types
+import type { PurchaseListSnapshot } from "./purchase-list";
 
 @Component({
   selector: 'app-purchase-component',
@@ -13,6 +19,26 @@ import { MatIconModule } from "@angular/material/icon";
     MatIconModule
   ],
 })
-export class PurchaseListComponent {
+export class PurchaseListComponent implements OnInit {
 
+  private purchaseListService = inject(PurchaseListService);
+
+  purchaseList = signal<PurchaseListSnapshot | null >(null);
+  loading = signal(true);
+  error = signal(false);
+
+  ngOnInit(): void {
+    this.purchaseListService.getPurchaseList().subscribe({
+      next: purchaseList => {
+        this.purchaseList.set(purchaseList);
+        this.error.set(false);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.purchaseList.set(null);
+        this.error.set(true);
+        this.loading.set(false);
+      }
+    });
+  }
 }
